fd0e47c211309732b5e7cc7d87f2b3c65e54abbc
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-02-15 15:11:02 +0800

    v1.0.1

--- main/main.go
+++ main/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"TXExec/exec"
-	"fmt"
-	"os"
-	"os/signal"
 )
 
 func main() {
-	c := make(chan os.Signal)
-	signal.Notify(c)
 	exec.Init()
-	<-c
-	fmt.Println("bye")
+	for {
+		continue
+	}
 }
