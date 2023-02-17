73cb63dd051851ca2c130f47e32c0a93216b2f0f
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-02-17 22:07:14 +0800

    test version

--- main/exec/tx.go
+++ main/exec/tx.go
@@ -5,7 +5,6 @@ import (
 	"encoding/gob"
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -104,19 +103,18 @@ func getNormalRandom() int {
 func GenTxSet() []*Tx {
 	n := config.BatchTxNum
 	m := config.OpsPerTx
-	valFormat := "%0" + strconv.Itoa(config.ValueSize) + "%s" // todo
+	//valFormat := "%0" + strconv.Itoa(config.ValueSize) + "%s" // todo
 	wrate := config.WRate
 	txs := make([]*Tx, n)
 	for i := range txs {
 		ops := make([]Op, m)
 		for j := range ops {
-			fmt.Println(j)
 			r := rand.Float64()
 			if r < wrate {
 				// 生成一笔写操作
 				ops[j].Type = OpWrite
 				ops[j].Key = getRandomKeyWithHot()
-				ops[j].Val = fmt.Sprintf(valFormat, uuid.NewString()) // todo
+				ops[j].Val = uuid.NewString()
 			} else {
 				// 生成一笔读操作
 				ops[j].Type = OpRead
