141239e90ab70389f9452665b4f0aed6cadaf144
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-02-19 13:46:42 +0800

    new commit

--- main/exec/peer.go
+++ main/exec/peer.go
@@ -85,6 +85,7 @@ func (peer *Peer) string() string {
 
 // 获取当前epoch中的{state: op->op}
 func (peer *Peer) getHashTable(id int, bias int) map[string][]Op {
+	fmt.Println(peer.id)
 	hashtable := make(map[string][]Op)
 	record := peer.record[id]
 	for i := 0; i < bias; i++ {
