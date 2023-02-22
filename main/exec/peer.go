b5db2fe6fd2be220525c7948c6ec8321add28c81
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-02-22 15:42:18 +0800

    add waiting

--- main/exec/peer.go
+++ main/exec/peer.go
@@ -3,6 +3,7 @@ package exec
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"strconv"
 	"sync"
@@ -222,7 +223,7 @@ func (peer *Peer) UpdateIndexToRecord(id int, bias int) {
 // 更新出块timeout
 func (peer *Peer) getNewBlockTimeout() {
 	peer.mu.Lock()
-	peer.blockTimeout = time.Duration(1000+peer.id*1000) * time.Millisecond
+	peer.blockTimeout = time.Duration(1000+rand.Intn(peer.id*1000)) * time.Millisecond
 	peer.blockTimeStamp = time.Now()
 	peer.mu.Unlock()
 }
