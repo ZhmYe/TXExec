4230fbe56ccde11107c3376c5dec27ff81371e8d
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-04-15 21:15:55 +0800

    new commit

--- main/exec/config.go
+++ main/exec/config.go
@@ -39,7 +39,7 @@ var config = Config{
 	ConflictMode:         false,
 	InstanceConflictRate: 0.2,
 	HotKey:               0.2,
-	HotKeyRate:           0.4,
+	HotKeyRate:           0.8,
 	StdDiff:              10000.0,
 	PeerNumber:           4,
 	execTimeout:          time.Duration(1) * time.Minute,
