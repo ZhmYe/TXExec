79226de3573429870a186cb84aad113f3f3576ce
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-04-19 16:55:52 +0800

    new commit

--- main/exec/config.go
+++ main/exec/config.go
@@ -30,15 +30,15 @@ type Config struct {
 }
 
 var config = Config{
-	BatchTxNum:           500,
+	BatchTxNum:           200,
 	ValueSize:            64,
 	OpsPerTx:             3,
-	OriginKeys:           5000,
+	OriginKeys:           10000,
 	WRate:                0.5,
 	ConflictMode:         false,
 	InstanceConflictRate: 0.2,
 	HotKey:               0.2,
-	HotKeyRate:           0.7,
+	HotKeyRate:           0.6,
 	StdDiff:              10000.0,
 	PeerNumber:           4,
 	execTimeout:          time.Duration(1) * time.Minute,
