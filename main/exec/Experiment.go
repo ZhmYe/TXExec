cf1146477cb2d47a1769ebd3a5397343654d1fa0
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-04-21 13:52:32 +0800

    new commit

--- main/exec/Experiment.go
+++ main/exec/Experiment.go
@@ -16,7 +16,7 @@ func SimpleTest() {
 	var peer = newPeer(peerId[0], Monitor, timestamp, peerId, saving, savingAmount, checking, checkingAmount, publicKey, hashed, signature)
 	blockNumber := make([]int, 4)
 	blockNumber[0] = 1
-	blockNumber[1] = 1
+	blockNumber[1] = 0
 	blockNumber[2] = 0
 	blockNumber[3] = 0
 	totalNumber := blockNumber[0] + blockNumber[1] + blockNumber[2] + blockNumber[3]
