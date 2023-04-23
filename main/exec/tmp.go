2ba43846c8c8ac6990626c650d2022961c4caaaa
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-04-23 12:48:07 +0800

    new commit

---
+++ main/exec/tmp.go
@@ -0,0 +1,24 @@
+package exec
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type test struct {
+	workload int64
+	nodeId   int64
+	number   int64
+}
+type msg struct {
+	data test
+	sign []byte
+}
+
+func tmpTest() {
+	Publickey, hashed, signature := getSignInfo()
+	fmt.Println(Publickey, hashed, signature)
+	testData := test{100, 10, 10}
+	testMsg := msg{testData, signature}
+	fmt.Print(unsafe.Sizeof(testMsg))
+}
