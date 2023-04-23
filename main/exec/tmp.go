b24172a0e4e3e34e4688bf1e3a8b244693ecf062
ECNUyzm542 <10195501426@stu.ecnu.edu.cn>
2023-04-23 12:57:41 +0800

    new commit

--- main/exec/tmp.go
+++ main/exec/tmp.go
@@ -1,6 +1,10 @@
 package exec
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
 	"fmt"
 	"unsafe"
 )
@@ -15,10 +19,31 @@ type msg struct {
 	sign []byte
 }
 
+func tmpGetSignInfo(testData test) (*rsa.PublicKey, [32]byte, []byte) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	publicKey := &privateKey.PublicKey
+
+	// 待签名的数据
+	//message := []byte("hello world")
+
+	// 计算消息摘要
+	message := *(*[]byte)(unsafe.Pointer(&testData))
+	hashed := sha256.Sum256(message)
+
+	// 对消息摘要进行数字签名
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed[:])
+	if err != nil {
+		panic(err)
+	}
+	return publicKey, hashed, signature
+}
 func TmpTest() {
-	_, _, signature := getSignInfo()
-	//fmt.Println(Publickey, hashed, signature)
 	testData := test{100, 10, 10}
+	_, _, signature := tmpGetSignInfo(testData)
+	//fmt.Println(Publickey, hashed, signature)
 	testMsg := msg{testData, signature}
 	fmt.Println(unsafe.Sizeof(testMsg))
 }
